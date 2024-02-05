a7364fb9ac30631cd65bc391f21e8cf98a467301
Caro Valverde <carolina.valverde255@gmail.com>
2024-02-05 18:32:45 -0300

    agrego handleSubmit

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -2,23 +2,31 @@ import { Button, Form } from "react-bootstrap";
 import Listatareas from "./Listatareas";
 import { useState } from "react";
 
-
 const FormularioTareas = () => {
-const [tarea, setTarea] = useState("");
-const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //guardar la tarea en el array
+    //spread
+    setTareas([...tareas, tarea]);
+  };
 
   return (
     <section>
-      <Form >
-        <Form.Group className="mb-3 d-flex" >
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex">
           <Form.Control
             type="text"
             placeholder="Ej: tarea 1"
             minLength={3}
             maxLength={50}
-            onChange={(e)=> setTarea(e.target.value)}
+            onChange={(e) => setTarea(e.target.value)}
           />
-          <Button variant="success" className="mx-2" type="submit">Enviar</Button>
+          <Button variant="success" className="mx-2" type="submit">
+            Enviar
+          </Button>
         </Form.Group>
       </Form>
       <Listatareas></Listatareas>
